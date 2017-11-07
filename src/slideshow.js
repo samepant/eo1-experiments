dc59c3eb14a4531ce96ab612de8964dbba7ceb0f
samepant <sampanter@gmail.com>
2017-11-06 21:55:11 -0500

    white and longert

--- src/slideshow.js
+++ src/slideshow.js
@@ -15,7 +15,7 @@ export default class extends React.Component {
   }
 
   componentDidMount () {
-    const imageChooser = setInterval(this.setNextImage, 10000)
+    const imageChooser = setInterval(this.setNextImage, 15000)
     this.fetchImageIds()
   }
 
