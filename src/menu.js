42824627669f5158918f58966d34bc70a23a1842
samepant <sampanter@gmail.com>
2017-11-12 09:41:20 -0500

    more omreom omroe more more more more

--- src/menu.js
+++ src/menu.js
@@ -7,17 +7,30 @@ export default class extends React.Component {
       <div className='menu'>
         <div>
           <h2>SMOOTHIES - <span className='price'>4% Li or $5</span></h2>
-          <span>flavors (pick two)</span>
-          <ul className='flavors'>
-            <li>strawberry</li>
-            <li>blueberry</li>
-            <li>mango</li>
-            <li>banana</li>
-          </ul>
+          <div className='smoothie-options'>
+            <div className='side1'>
+              <span>flavors (pick two)</span>
+              <ul className='flavors'>
+                <li>strawberry</li>
+                <li>blueberry</li>
+                <li>mango</li>
+                <li>banana</li>
+              </ul>
+            </div>
+            <div className='side2'>
+              <span>add ons</span>
+              <ul className='flavors'>
+                <li>protein (+2)</li>
+                <li>lithium (free)</li>
+                <li>byo (+1)</li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div>
           <h2>Lithium Exchange</h2>
-          <span className='price'>$1 == 1% Li</span>
+          <p className='price'>$1 == 1% Li</p>
+          <p>we accept: cash/credit/crypto</p>
         </div>
       </div>
     )
